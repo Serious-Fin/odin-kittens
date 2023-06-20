075a0e200ce84c1bb7a5898a12b207da694d71e5
Serious-Fin <augustasgri@gmail.com>
2023-06-20 15:11:10 +0300

    Add #new and #create actions to kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -6,4 +6,25 @@ class KittensController < ApplicationController
   def show
     @kitten = Kitten.find(id=params[:id])
   end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:alert] = "Kitten successfully created"
+      redirect_to(@kitten)
+    else
+      flash.now[:error] = "Please fill out the form"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,4 +1,10 @@
 class Kitten < ApplicationRecord
+
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :cuteness, presence: true
+  validates :softness, presence: true
+
   def kitten_info
     "Name: #{self.name} | Age: #{self.age} | Cuteness: #{self.cuteness} | Softness: #{self.softness}"
   end
